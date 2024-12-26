4dcf993a666c7f2077f5ff0d8132c6e242958d86
chetan_anna <achetan217@gmail.com>
2024-12-26 17:47:28 +0530

    Removed duplciate folder

---
+++ sam/src/CollectionDemo/ArraytoCollection123.java
@@ -0,0 +1,23 @@
+package CollectionDemo;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ArraytoCollection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String s1[]={"ratan","shravya","aruna"};
+ArrayList<String>al=new ArrayList<>(Arrays.asList(s1));
+al.add("s4");
+for (String string : al) {
+	System.out.println(string);
+}
+LinkedList<String>ll=new LinkedList<>(Arrays.asList(s1));
+for (String string : ll) {
+	System.out.println(string);
+}
+	}
+
+}
